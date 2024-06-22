ad27a5d6f030eb36f794162649bbad4d6b7a0126
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-06-22 01:10:39 -0500

    funciones de el tipado con estilos

--- javascript-100-projects/03-typing-game/script.js
+++ javascript-100-projects/03-typing-game/script.js
@@ -19,14 +19,20 @@ function initGame() {
   currentTime = INITIAL_TIME;
 
   $time.textContent = currentTime;
+  console.log(
+    words.map((word) => {
+      return word;
+    })
+  );
+  $paragraph.innerHTML = words
+    .map((word, index) => {
+      const letter = word.split('');
 
-  $paragraph.innerHTML = words.map((word, index) => {
-    const letter = word.split('');
-
-    return `<x-word>${letter
-      .map((letter) => `<x-letter>${letter}</x-letter>`)
-      .join('')}</x-word>`;
-  });
+      return `<x-word>${letter
+        .map((letter) => `<x-letter>${letter}</x-letter>`)
+        .join('')}</x-word>`;
+    })
+    .join('');
 
   const $firstWord = $paragraph.querySelector('x-word');
 
@@ -43,5 +49,77 @@ function initGame() {
     }
   }, 1000);
 }
-function initEvents() {}
+function initEvents() {
+  document.addEventListener('keydown', () => {
+    $input.focus();
+  });
+
+  $input.addEventListener('keydown', onKeyDown);
+  $input.addEventListener('keyup', onKeyUp);
+}
+
+function onKeyDown(event) {
+  const $currentWord = $paragraph.querySelector('x-word.active');
+  const $currentLetter = $paragraph.querySelector('x-letter.active');
+
+  const {key} = event;
+
+  if (key === ' ') {
+    event.preventDefault();
+    const $nextWord = $currentWord.nextElementSibling;
+    const $nextLetter = $nextWord.querySelector('x-letter');
+
+    $currentWord.classList.remove('active', 'marked');
+    $currentLetter.classList.remove('active');
+
+    $nextWord.classList.add('active');
+    $nextLetter.classList.add('active');
+
+    $input.value = '';
+
+    const hasMissedLetters =
+      $currentWord.querySelectorAll('x-letter:not(.correct)').length > 0;
+
+    const classToAdd = hasMissedLetters ? 'marked' : 'correct';
+    $currentWord.classList.add(classToAdd);
+  }
+}
+function onKeyUp() {
+  // recuperamos los elementos actuales
+  const $currentWord = $paragraph.querySelector('x-word.active');
+  const $currentLetter = $paragraph.querySelector('x-letter.active');
+
+  const currentWord = $currentWord.innerText.trim();
+  $input.maxLength = currentWord.length;
+  console.log({
+    value: $input.value,
+    currentWord,
+  });
+
+  const $allLetters = $currentWord.querySelectorAll('x-letter');
+
+  $allLetters.forEach(($letter) =>
+    $letter.classList.remove('correct', 'incorrect')
+  );
+
+  $input.value.split('').forEach((char, index) => {
+    const $letter = $allLetters[index];
+    const letterToCheck = currentWord[index];
+
+    const isCorrect = char === letterToCheck;
+    const letterClass = isCorrect ? 'correct' : 'incorrect';
+    $letter.classList.add(letterClass);
+  });
+
+  $currentLetter.classList.remove('active', 'is-last');
+  const inputLength = $input.value.length;
+  const $nextActiveLetter = $allLetters[inputLength];
+
+  if ($nextActiveLetter) {
+    $nextActiveLetter.classList.add('active');
+  } else {
+    $currentLetter.classList.add('active', 'is-last');
+  }
+}
+
 function gameOver() {}
