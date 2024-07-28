a69bad7b2a95b6649e99342eb9074bd18e6738ec
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-07-27 21:33:04 -0500

    finish 3D model section: look Sentry and finish feature section

--- JavaScript-Mastery/apple_website/src/main.jsx
+++ JavaScript-Mastery/apple_website/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://27c12e2727998eca9354fa1e04e1e962@o4507675956609024.ingest.us.sentry.io/4507675961065472',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
